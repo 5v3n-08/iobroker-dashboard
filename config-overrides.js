8213a94847b2a5ed8be3bb71a4645638a97e75e9
Sven Stiels <sven.stiels@zalando.de>
2021-05-02 14:22:11 +0200

    eslint fixes

--- config-overrides.js
+++ config-overrides.js
@@ -1,22 +1,23 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-const { override, fixBabelImports, addLessLoader, useBabelRc, addWebpackAlias, addBabelPlugins } = require("customize-cra");
-
+const { override, fixBabelImports, addLessLoader, useBabelRc, addWebpackAlias, addBabelPlugins } = require('customize-cra');
 
 module.exports = override(
   useBabelRc(),
-  ...addBabelPlugins('@babel/plugin-proposal-nullish-coalescing-operator','@babel/plugin-proposal-optional-chaining'),
-  addWebpackAlias({environment: path.join(__dirname, 'src', 'environments', process.env.CLIENT_ENV)}),
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
+  ...addBabelPlugins('@babel/plugin-proposal-nullish-coalescing-operator', '@babel/plugin-proposal-optional-chaining'),
+  addWebpackAlias({ environment: path.join(__dirname, 'src', 'environments', process.env.CLIENT_ENV) }),
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
     style: true,
   }),
   addLessLoader({
     lessOptions: {
       javascriptEnabled: true,
       modifyVars: {
-        "@heading-color": "default",
-        "@body-background": "default",
+        '@heading-color': 'default',
+        '@body-background': 'default',
       },
     },
   })
--- src/helpers/FontAwesome.ts
+++ src/helpers/FontAwesome.ts
@@ -2,4 +2,4 @@ import _ from 'lodash';
 import * as icons from '@fortawesome/free-solid-svg-icons';
 
 export const fasIcons: string[] = _.map(icons, (icon) => icon['iconName']);
-export const isFasIcon = (value: string | undefined) => value !== undefined && fasIcons.includes(value);
+export const isFasIcon = (value: string | undefined): boolean => value !== undefined && fasIcons.includes(value);
