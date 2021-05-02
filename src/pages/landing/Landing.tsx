69c10ae33788e3813066e3f2e18dc8a4e6e29de2
Sven Stiels <sven.stiels@zalando.de>
2021-05-02 18:17:27 +0200

    [sstiels] update landing

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,4 +1,5 @@
-import { HueBlub } from 'components/hue-blub/HueBlub';
+import { HueLamp } from 'components/iobroker/hue-lamp/HueLamp';
+import { PowerSwitch } from 'components/iobroker/power-switch/PowerSwitch';
 import { TrashSchedulerOnlyNext } from 'components/iobroker/trash-scheduler/TrashSchedulerOnlyNext';
 import React, { Fragment } from 'react';
 
@@ -6,12 +7,13 @@ interface IProps {}
 const Landing: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => {
   return (
     <Fragment>
-      <div className="row">
-        <HueBlub className="col-2" identifier="hue.0.Büro_PC_rechts" name="PC - rechts" room="Büro" />
-        <HueBlub className="col-2" identifier="hue.0.Büro_PC_links" name="PC - links" room="Büro" />
+      <div className="row mb-2">
+        <TrashSchedulerOnlyNext identifier="trashschedule.0" />
       </div>
       <div className="row">
-        <TrashSchedulerOnlyNext identifier="trashschedule.0" />
+        <HueLamp className="col-2 mr-1" identifier="hue.0.Büro_PC_links" name="PC - links" room="Büro" />
+        <HueLamp className="col-2 mx-1" identifier="hue.0.Büro_PC_rechts" name="PC - rechts" room="Büro" />
+        <PowerSwitch className="col-2 mx-1" identifier="hue.0.Büro_PC_rechts.on" iconSet="switch" name="PC - rechts" room="Büro" />
       </div>
     </Fragment>
   );
