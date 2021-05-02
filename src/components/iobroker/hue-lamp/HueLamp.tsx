dcbc0e3a71c9867c22c6ac588b55889ef1486fc2
Sven Stiels <sven.stiels@zalando.de>
2021-05-02 15:43:57 +0200

    [hue-lamp] Add useLongPress for demo

--- src/components/iobroker/hue-lamp/HueLamp.tsx
+++ src/components/iobroker/hue-lamp/HueLamp.tsx
@@ -1,11 +1,10 @@
-import { Card } from 'antd';
 import React, { useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useWebsocket } from 'hooks/useWebsocket';
 import { useObject } from 'hooks/useObject';
-import { ReactSVG } from 'react-svg';
 import lightbulbOn from 'assets/svg/lightbulb_on.svg';
 import lightbulbOff from 'assets/svg/lightbulb_off.svg';
+import { Avatar, Card } from 'antd';
+import Meta from 'antd/lib/card/Meta';
+import { useLongPress } from 'hooks/useLongPress';
 
 interface IProps {
   name: string;
@@ -18,11 +17,29 @@ export const HueLamp: React.FC<IProps> = (props: React.PropsWithChildren<IProps>
   const on = useObject<boolean>(`${identifier}.on`);
   const bri = useObject<number>(`${identifier}.bri`);
 
+  const onLongPress = () => {
+    console.log('long pressed');
+  };
+  const onClick = () => {
+    on.setValue(!on.value);
+  };
+  const longPressEvent = useLongPress(onClick, onLongPress, {
+    shouldPreventDefault: true,
+    delay: 300,
+  });
+
   return (
-    <Card className={[className, 'text-center'].join(' ')} onClick={() => on.setValue(!on.value)}>
-      <small className="text-muted">{props.room}</small>
-      <div className="text-center">{props.name}</div>
-      <ReactSVG className="d-flex align-items-center" style={{ width: '50px' }} src={on.value ? lightbulbOn : lightbulbOff} />
+    <Card
+      // onClick={(e) => on.setValue(!on.value)}
+      {...longPressEvent}
+      cover={<img className="text-center" src={lightbulbOn} style={{ height: 50, width: 50 }} />}
+      actions={[<span key="1">Test1</span>, <span key="2">Test1</span>]}
+    >
+      <Meta
+        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+        title="Card title"
+        description="This is the description"
+      />
     </Card>
   );
 };
