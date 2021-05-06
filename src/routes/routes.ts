4f69ef9043d4690a7d5843ad6d9332310443819a
Sven Stiels <sstiels@zalando-62110.fritz.box>
2021-05-06 18:59:17 +0200

    [update] clear old routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -15,16 +15,4 @@ export const routes: IRoute[] = [
     onSidenav: true,
     component: () => lazy(() => import('pages/badezimmer/Badezimmer')),
   },
-  {
-    path: '/bigalke',
-    name: 'Bigalke',
-    onSidenav: false,
-    component: () => lazy(() => import('pages/bigalke/Landing')),
-  },
-  {
-    path: '/roborock',
-    name: 'Roborock',
-    onSidenav: false,
-    component: () => lazy(() => import('components/iobroker/roborock/components/widget/full/RoborockWidgetCardFull')),
-  },
 ];
