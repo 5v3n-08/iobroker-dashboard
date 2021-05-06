696229239703a6004de8990a1f1ca08f6fb81966
Michael Bigalke <bigalkemichael@gmail.com>
2021-05-06 12:03:08 +0200

    [Roborock] History dynamic

---
+++ src/components/iobroker/roborock/components/RoborockConsumables.tsx
@@ -0,0 +1,67 @@
+import { Progress } from 'antd';
+import { useObject } from 'hooks/useObject';
+import _ from 'lodash';
+import React, { Fragment, useEffect, useState } from 'react';
+
+interface IProps {
+  identifier: string;
+  consumables?: IRoborockConsumables;
+}
+
+const CConsumableTitle = {
+  filter: 'Filter',
+  main_brush: 'Hauptbürste',
+  sensors: 'Sensoren',
+  side_brush: 'Seitenbürste',
+  water_filter: 'Wasserfilter',
+};
+
+export const RoborockConsumables: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => {
+  const { identifier, consumables } = props;
+
+  // Get consumables
+  const consumFilter = useObject<number>(`${identifier}.consumable.filter`);
+  const consumMainBrush = useObject<number>(`${identifier}.consumable.main_brush`);
+  const consumSensors = useObject<number>(`${identifier}.consumable.sensors`);
+  const consumSideBrush = useObject<number>(`${identifier}.consumable.side_brush`);
+  const consumWaterFilter = useObject<number>(`${identifier}.consumable.water_filter`);
+
+  return (
+    <Fragment>
+      {_.each(consumables, (active, consumableType) => {
+        const consum =
+          consumableType === 'filter'
+            ? consumFilter
+            : consumableType === 'main_brush'
+            ? consumMainBrush
+            : consumableType === 'sensors'
+            ? consumSensors
+            : consumableType === 'side_brush'
+            ? consumSideBrush
+            : consumWaterFilter;
+        consum.value <= 10 && (
+          <div>
+            <div className="small text-muted">{consum.value}</div>
+            <Progress
+              //   percent={consum.value}
+              size="small"
+              strokeColor={{
+                '0%': '#FF0000',
+                '50%': '#ffc107',
+                '100%': '#87d068',
+              }}
+            />
+          </div>
+        );
+      })}
+    </Fragment>
+  );
+};
+
+export interface IRoborockConsumables {
+  filter?: boolean;
+  main_brush?: boolean;
+  sensors?: boolean;
+  side_brush?: boolean;
+  water_filter?: boolean;
+}
--- src/components/iobroker/roborock/components/widget/full/RoborockWidgetCardFull.tsx
+++ src/components/iobroker/roborock/components/widget/full/RoborockWidgetCardFull.tsx
@@ -12,6 +12,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import _ from 'lodash';
 import { CaretRightOutlined, PauseOutlined, HomeOutlined } from '@ant-design/icons';
 import { useLongPress } from 'hooks/useLongPress';
+import { IRoborockConsumables, RoborockConsumables } from '../../RoborockConsumables';
 
 interface IProps {
   title?: string;
@@ -26,6 +27,7 @@ interface IProps {
     maintenance?: boolean;
     statistics?: boolean;
     actions?: boolean;
+    consumables?: IRoborockConsumables;
   };
 }
 export const RoborockWidgetCardFull: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => {
@@ -36,7 +38,30 @@ export const RoborockWidgetCardFull: React.FC<IProps> = (props: React.PropsWithC
   // const rooms = roborockRoomIds
   const rooms = useObject<string[]>(`${identifier}.rooms`);
 
+  console.log('writing to localStorage');
+  localStorage.setItem('test', 'testValue');
+  const consumables: IRoborockConsumables = {};
   // consumables
+  if (props.components?.consumables?.filter === undefined || props.components?.consumables?.filter !== false) {
+    consumables.filter = true;
+    // consumables.push('filter');
+  }
+  if (props.components?.consumables?.main_brush === undefined || props.components?.consumables?.main_brush !== false) {
+    consumables.main_brush = true;
+    // consumables.push('main_brush');
+  }
+  if (props.components?.consumables?.sensors === undefined || props.components?.consumables?.sensors !== false) {
+    consumables.sensors = true;
+    // consumables.push('sensors');
+  }
+  if (props.components?.consumables?.side_brush === undefined || props.components?.consumables?.side_brush !== false) {
+    consumables.side_brush = true;
+    // consumables.push('side_brush');
+  }
+  if (props.components?.consumables?.water_filter === undefined || props.components?.consumables?.water_filter !== false) {
+    consumables.water_filter = true;
+    // consumables.push('water_filter');
+  }
   const consumFilter = useObject<number>(`${identifier}.consumable.filter`);
   const consumMain = useObject<number>(`${identifier}.consumable.main_brush`);
   const consumSensors = useObject<number>(`${identifier}.consumable.sensors`);
@@ -109,6 +134,7 @@ export const RoborockWidgetCardFull: React.FC<IProps> = (props: React.PropsWithC
             <Row className="mt-4">
               {(props.components?.maintenance === undefined || props.components.maintenance !== false) && (
                 <Col>
+                  <RoborockConsumables consumables={consumables} identifier={identifier} />
                   {/* <Card title="Wartung" size="small"> */}
                   {consumFilter.value <= 10 && (
                     <div>
