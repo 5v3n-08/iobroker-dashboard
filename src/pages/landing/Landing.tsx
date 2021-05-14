4b8ba2cb9a131ac22eeb0f2e5674f6e377dff079
Sven Stiels <sstiels@zalando-62110.fritz.box>
2021-05-14 14:12:42 +0200

    Update Landing.tsx

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,20 +1,18 @@
 import { HueLamp } from 'components/iobroker/hue-lamp/HueLamp';
-import { PowerSwitch } from 'components/iobroker/power-switch/PowerSwitch';
-import { WashingMachineCountdown } from 'components/iobroker/washing-machine-countdown/WashingMachineCountdown';
 import { TrashSchedulerOnlyNext } from 'components/iobroker/trash-scheduler/TrashSchedulerOnlyNext';
 import React, { Fragment } from 'react';
 import { MyTime } from 'components/iobroker/my-time/MyTime';
-import { Todoist } from 'components/iobroker/todoist/Todoist';
-import { WebsocketStatus } from 'components/websocket-status/WebsocketStatus';
 
 interface IProps {}
 const Landing: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => {
   return (
     <Fragment>
-      <div className="row">
-        <WashingMachineCountdown name="Waschmaschiene" hours={3} minutes={30} />
+      <div className="row m-2">
+        <TrashSchedulerOnlyNext className="mx-1" identifier="trashschedule.0" />
+        <HueLamp className="mx-1" name="Flur" identifier="hue.0.Flur" />
+      </div>
+      <div className="row  m-2">
         <MyTime identifier="mytime.0.Countdown.test" />
-        <WebsocketStatus />
       </div>
     </Fragment>
   );
