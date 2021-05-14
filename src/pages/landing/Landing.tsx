631087bf78ca2b473f2255eb916341fb39c83b1e
Sven Stiels <sstiels@zalando-62110.fritz.box>
2021-05-14 13:57:18 +0200

    [landing] Add test widgets

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -3,6 +3,9 @@ import { PowerSwitch } from 'components/iobroker/power-switch/PowerSwitch';
 import { WashingMachineCountdown } from 'components/iobroker/washing-machine-countdown/WashingMachineCountdown';
 import { TrashSchedulerOnlyNext } from 'components/iobroker/trash-scheduler/TrashSchedulerOnlyNext';
 import React, { Fragment } from 'react';
+import { MyTime } from 'components/iobroker/my-time/MyTime';
+import { Todoist } from 'components/iobroker/todoist/Todoist';
+import { WebsocketStatus } from 'components/websocket-status/WebsocketStatus';
 
 interface IProps {}
 const Landing: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => {
@@ -10,9 +13,12 @@ const Landing: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => {
     <Fragment>
       <div className="row mb-2">
         <TrashSchedulerOnlyNext identifier="trashschedule.0" />
+        <Todoist className="col-5" identifier="todoist2.0" project="Inbox" />
       </div>
       <div className="row">
         <WashingMachineCountdown name="Waschmaschiene" hours={3} minutes={30} />
+        <MyTime identifier="mytime.0.Countdown.test" />
+        <WebsocketStatus />
       </div>
     </Fragment>
   );
