67616819e8cc410f2f6a0f8ff5bef5f2254f2bf4
Sven Stiels <sstiels@zalando-62110.fritz.box>
2021-05-15 02:44:29 +0200

    [landing] Add grid-layout in playground

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,23 +1,8 @@
-import { HueLamp } from 'components/iobroker/hue-lamp/HueLamp';
-import { PowerSwitch } from 'components/iobroker/power-switch/PowerSwitch';
-import { WashingMachineCountdown } from 'components/iobroker/washing-machine-countdown/WashingMachineCountdown';
-import { TrashSchedulerOnlyNext } from 'components/iobroker/trash-scheduler/TrashSchedulerOnlyNext';
 import React, { Fragment } from 'react';
-import { MyTime } from 'components/iobroker/my-time/MyTime';
-import { Todoist } from 'components/iobroker/todoist/Todoist';
-import { WebsocketStatus } from 'components/websocket-status/WebsocketStatus';
 
 interface IProps {}
 const Landing: React.FC<IProps> = (props: React.PropsWithChildren<IProps>) => {
-  return (
-    <Fragment>
-      <div className="row">
-        <WashingMachineCountdown name="Waschmaschiene" hours={3} minutes={30} />
-        <MyTime identifier="mytime.0.Countdown.test" />
-        <WebsocketStatus />
-      </div>
-    </Fragment>
-  );
+  return <Fragment>Landing</Fragment>;
 };
 
 export default Landing;
